ffc8f0b9fcab6cb23d16c219143ed90bcaafe1cd
Sergiyan92 <sergiyan@ukr.net>
2023-10-03 17:11:41 +0300

    make answers

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,21 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function isWeekend(day: Day): boolean {
+  return day === Day.Sunday || day === Day.Saturday;
+}
+
+const today: Day = Day.Saturday;
+const isTodayWeekend: boolean = isWeekend(today);
+console.log(isTodayWeekend); // Виведе true, оскільки субота - це вихідний день
--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,34 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+function compare<T extends AllType, U extends AllType>(
+  top: Pick<T, keyof AllType>,
+  bottom: Pick<U, keyof AllType>
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
+const obj1: Pick<AllType, keyof AllType> = {
+  name: "Object 1",
+  position: 1,
+  color: "red",
+  weight: 10,
+};
 
+const obj2: Pick<AllType, keyof AllType> = {
+  name: "Object 2",
+  position: 2,
+  color: "blue",
+  weight: 20,
+};
+
+const result = compare(obj1, obj2);
+console.log(result);
 export {};
